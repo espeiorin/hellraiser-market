be2928150a6cf5dc31e50a8fda23472c30d2713f
Guilherme Defreitas Juraszek <guilherme.defreitas@gmail.com>
2014-05-27 16:03:14 -0300

    Initial Commit

---
+++ HellraiserMarket/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HellraiserMarket
+//
+//  Created by Guilherme on 5/27/14.
+//  Copyright (c) 2014 guidefreitas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
