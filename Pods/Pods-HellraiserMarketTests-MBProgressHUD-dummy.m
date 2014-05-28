8349fcb074f3aad6f4b550aa8436dd010c368fca
Guilherme Defreitas Juraszek <guilherme.defreitas@gmail.com>
2014-05-28 01:56:13 -0300

    Funcionando para iPad e iPhone

---
+++ Pods/Pods-HellraiserMarketTests-MBProgressHUD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HellraiserMarketTests_MBProgressHUD : NSObject
+@end
+@implementation PodsDummy_Pods_HellraiserMarketTests_MBProgressHUD
+@end
